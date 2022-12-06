1cddd6fb66e3769d1814e64ae0565261fed9fb9b
Jānis Alunāns <j.alunans@gmail.com>
2022-12-06 19:40:50 +0200

    Add victory conditions

--- script.js
+++ script.js
@@ -1,3 +1,6 @@
+// Updates the state of the gameboard and informs you of the state it is in
+// E.g. whether move is legal, where the tokens are places
+// and whether victory or tie been achieved
 const gameBoard = (function () {
   const generateBoard = function () {
     return [[null, null, null],
@@ -36,5 +39,47 @@ const gameBoard = (function () {
     board = generateBoard();
   }
 
-  return { putSymbol, clear, board }
+  const allSame = function (arr) {
+    return arr.every(e=>arr[0] === e)
+  };
+
+  const noNullFoundIn = function (arr) {
+    return arr.every(e=>e !== null);
+  }
+
+  const noBlanks = function () {
+    return board.every(row=>noNullFoundIn(row));
+  }
+
+  // turns rown into columns
+  const transpose = function(arr) {
+    return Object.keys(arr[0]).map(function(c) {
+        return arr.map(function(r) { return r[c]; });
+    });
+}
+
+  const checkHorizontalWin = function (board) {
+    return board.some(row=>allSame(row) && noNullFoundIn(row));
+  };
+
+  const checkVerticalWin = function () {
+    let rotatedBoard = transpose(board);
+    return checkHorizontalWin(rotatedBoard);
+  }
+
+  const checkDiagonalWin = function () {
+    leftDiag = [board[0][0],board[1][1], board[2][2]];
+    rightDiag = [board[0][2], board[1][1], board[2][0]];
+    return checkHorizontalWin([leftDiag,rightDiag]);
+  }
+
+  const isVictory = function () {
+    return checkHorizontalWin(board) || checkVerticalWin() || checkDiagonalWin()
+  }
+
+  const isTie = function () {
+    return noBlanks() && !isVictory();
+  };
+
+  return { putSymbol, clear, board, isTie, isVictory}
 })();
